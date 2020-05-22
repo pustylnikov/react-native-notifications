ef2d83ac391b39b209a4ff27ffb5fa610019818e
pustylnikov <yura.pustylnikov@gmail.com>
2020-05-22 12:44:03 +0300

    config project

---
+++ README.md
@@ -0,0 +1,22 @@
+### Install
+using npm
+```text
+npm install @anvilapp/react-native-notifications --save
+```
+or using yarn
+```text
+yarn add @anvilapp/react-native-notifications
+```
+
+### Usage example
+```js
+import Notifications from '@anvilapp/react-native-notifications';
+
+const notificationsRef = React.createRef();
+
+<Notifications
+    ref={notificationsRef}
+    ...
+/>
+
+```
--- example/App.tsx
+++ example/App.tsx
@@ -1,16 +1,41 @@
 import React from 'react';
-import {SafeAreaView, StatusBar, StyleSheet} from 'react-native';
+import {View, SafeAreaView, StatusBar, StyleSheet, Text, Button} from 'react-native';
 
 const App = () => {
     return (
         <>
             <StatusBar barStyle="dark-content"/>
-            <SafeAreaView>
-            </SafeAreaView>
+            <SafeAreaView/>
+            <View style={styles.containerView}>
+                <Text style={styles.titleText}>React Native Notifications </Text>
+                <View style={styles.contentView}>
+                    <Button
+                        title="Show first notification"
+                        onPress={() => {
+                            // TODO
+                        }}
+                    />
+                </View>
+            </View>
         </>
     );
 };
 
-const styles = StyleSheet.create({});
+const styles = StyleSheet.create({
+    containerView: {
+        flex: 1,
+        padding: 15,
+    },
+    titleText: {
+        fontSize: 18,
+        color: '#000',
+        textAlign: 'center',
+    },
+    contentView: {
+        flex: 1,
+        justifyContent: 'center',
+        marginHorizontal: 20,
+    },
+});
 
 export default App;
