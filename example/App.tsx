fa8e4549bf2f687e14d3fccbda73d2b9b3b2f4b5
pustylnikov <yura.pustylnikov@gmail.com>
2020-05-22 12:20:52 +0300

    init commit

---
+++ example/App.tsx
@@ -0,0 +1,16 @@
+import React from 'react';
+import {SafeAreaView, StatusBar, StyleSheet} from 'react-native';
+
+const App = () => {
+    return (
+        <>
+            <StatusBar barStyle="dark-content"/>
+            <SafeAreaView>
+            </SafeAreaView>
+        </>
+    );
+};
+
+const styles = StyleSheet.create({});
+
+export default App;
